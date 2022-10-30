bd6811865721ca0eaf58bbfd087550d23f60dc1c
indegser <indegser@gmail.com>
2022-10-30 22:53:08 +0900

    feat: Update cors middleware to handle preflight request

--- middleware.ts
+++ middleware.ts
@@ -6,9 +6,13 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   const headers = new Headers(request.headers)
   const origin = request.headers.get('origin')
-
+  
+  /** @todo Authorization with jwt token */
   if (origin?.includes('indegser.com')) {
     headers.set('Access-Control-Allow-Origin', origin)
+    headers.set('Access-Control-Request-Headers', '*')
+    headers.set('Access-Control-Allow-Credentials', 'true')
+    headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   }
 
   return NextResponse.next({
